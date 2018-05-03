421d883df69a80ebc16e51b3647a19cf88357e39
R2 <r2@localhost.localdomain>
2018-05-03 17:42:08 -0300

    -

--- script.py
+++ script.py
@@ -1,20 +1,26 @@
 #import matplotlib.pyplot as plt
 import math
 
-k = [0.25, 0.025, 0.0025, 0.00025]
+k = [0.25, 0.025, 0.0025, 0.00025, 0.000012]
 peoples = 5500.0
-rn = 4.0
-n = 1
+rn = 4
 
-def rn_1(rn):
+def rn_1(rn,k):
     return (rn + k*rn*(peoples-rn))
 
-while(1):
-    if math.floor(rn) != math.ceil(rn_1(rn)):
-        n += 1
-    else:
-        break
-    rn = rn_1(rn)
-    print('numero d dias',math.ceil(rn))
 
-print('numero de dias', n)
+def rumors(k,rn):
+    print("k = ",k, " rn = ", rn)
+    n = 1    
+    while(1):
+        if rn_1(rn,k) -rn > 1:
+            n += 1
+        else:
+            break
+        rn = rn_1(rn,k)
+        print(n,' - numero d pessoas',math.ceil(rn))
+    
+    print('numero de dias', n)
+   
+for i in k:
+    rumors(i,rn)
