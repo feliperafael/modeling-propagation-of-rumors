3430781750d2161f185f0ea1ade1c4f39a972333
Felipe <felipe@localhost.localdomain>
2018-05-04 00:50:34 -0300

    added initial plot function

--- script.py
+++ script.py
@@ -1,26 +1,33 @@
-#import matplotlib.pyplot as plt
-import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 k = [0.25, 0.025, 0.0025, 0.00025, 0.000012]
-peoples = 5500.0
-rn = 4
+people = 5500.0 #number of people in company
+rn = 4          # number of people who heard rumor
+rns = []
 
 def rn_1(rn,k):
-    return (rn + k*rn*(peoples-rn))
+    return (rn + k*rn*(people-rn))
+
+def plot(rns):
+    x = np.arange(len((rns)))
+    plt.plot(x, rns)
+    plt.show()
 
 
 def rumors(k,rn):
     print("k = ",k, " rn = ", rn)
-    n = 1    
+    days = 1    
     while(1):
         if rn_1(rn,k) -rn > 1:
-            n += 1
+            days += 1
         else:
             break
         rn = rn_1(rn,k)
-        print(n,' - numero d pessoas',math.ceil(rn))
-    
-    print('numero de dias', n)
+        rns.append(rn)
+        print('day: ', days,' number of people: ',(rn))
+    plot(rns)
+    print('number of days', days)
    
 for i in k:
     rumors(i,rn)
